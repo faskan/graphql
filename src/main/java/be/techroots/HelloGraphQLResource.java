2ace87fd0abe684cb131dd1e67553afb88dc2ef3
Khan Faisal <faisal.khan@telenetgroup.be>
2024-12-05 17:04:57 +0100

    try sealed interface and records

--- src/main/java/be/techroots/HelloGraphQLResource.java
+++ src/main/java/be/techroots/HelloGraphQLResource.java
@@ -5,12 +5,26 @@ import org.eclipse.microprofile.graphql.Description;
 import org.eclipse.microprofile.graphql.GraphQLApi;
 import org.eclipse.microprofile.graphql.Query;
 
+import java.util.List;
+
 @GraphQLApi
 public class HelloGraphQLResource {
 
     @Query
-    @Description("Say hello")
-    public String sayHello(@DefaultValue("World") String name) {
-        return "Hello " + name;
+    public List<CustomerProduct> getProducts() {
+
+        return List.of(new InternetLine("Test", 1000), new MobileLine("Test", "0470000000"));
     }
+}
+
+sealed interface CustomerProduct permits InternetLine, MobileLine {
+    String name();
+}
+
+record InternetLine(String name, int speed) implements CustomerProduct {
+
+}
+
+record MobileLine(String name, String mobileNumber) implements CustomerProduct {
+
 }
