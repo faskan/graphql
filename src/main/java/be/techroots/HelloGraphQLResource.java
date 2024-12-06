08da14e0f29fb515786c4306ebfc59748c5998cb
Khan Faisal <faisal.khan@telenetgroup.be>
2024-12-06 09:24:25 +0100

    try sealed interface and records

--- src/main/java/be/techroots/HelloGraphQLResource.java
+++ src/main/java/be/techroots/HelloGraphQLResource.java
@@ -1,7 +1,5 @@
 package be.techroots;
 
-import org.eclipse.microprofile.graphql.DefaultValue;
-import org.eclipse.microprofile.graphql.Description;
 import org.eclipse.microprofile.graphql.GraphQLApi;
 import org.eclipse.microprofile.graphql.Query;
 
